e23cea97d5ed735b240525cf5810d30aa3e9e60c
Yves Morais Jaques <ymjaques@stefanini.com>
2021-08-17 11:03:21 -0300

    test: Ensure local purchase old cache deletion on save

---
+++ src/data/usecases/localSavePurchases.spec.ts
@@ -0,0 +1,35 @@
+class LocalSavePurchases {
+    constructor (private readonly cacheStore: CacheStore) {}
+
+    async save (): Promise<void> {
+        this.cacheStore.delete()
+    }
+}
+
+class CacheStore {
+    delete: () => void
+}
+
+class CacheStoreSpy implements CacheStore {
+    deleteCallsCount = 0
+    
+    delete (): void {        
+        this.deleteCallsCount++
+    }    
+}
+
+//sut = system under test
+describe('Save local purchases', () => {
+    test('Should not delete cache on sut.init', () => {
+        const cacheStore = new CacheStoreSpy()
+        new LocalSavePurchases(cacheStore)
+        expect(cacheStore.deleteCallsCount).toBe(0)
+    })
+    
+    test('Should delete old cache on sut.save', async () => {
+        const cacheStore = new CacheStoreSpy()
+        const sut = new LocalSavePurchases(cacheStore)
+        await sut.save()
+        expect(cacheStore.deleteCallsCount).toBe(1)
+    })    
+})
